8f5240a82b917825a593c109b2374cba6915f55b
tyrionzt <40282946+tyrionzt@users.noreply.github.com>
2019-03-10 14:38:20 +0800

    基础版

---
+++ resume/views.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.shortcuts import render
+from django.http import HttpResponse
+
+# Create your views here.
+def my_resume(request):
+    return render(request, 'resume.html')
+
+def download_resuem(request):
+    return HttpResponse("敬请期待~")
