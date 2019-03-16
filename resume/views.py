130c949e073d22248cbddfd470b3e17642cf524a
tyrionzt <40282946+tyrionzt@users.noreply.github.com>
2019-03-16 16:04:37 +0800

    UI优化，增加下载简历地址

--- resume/views.py
+++ resume/views.py
@@ -3,10 +3,15 @@ from __future__ import unicode_literals
 
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.http import StreamingHttpResponse
 
 # Create your views here.
 def my_resume(request):
     return render(request, 'resume.html')
 
 def download_resuem(request):
-    return HttpResponse("404")
+    file = open('/home/amarsoft/download/example.tar.gz', 'rb')
+    response = HttpResponse(file)
+    response['Content-Type'] = 'application/octet-stream'
+    response['Content-Disposition'] = 'attachment;filename="简历.zip"'
+    return response
