40a6d47dab15206b188f99ee1dbf4984ce1def22
tyrionzt <40282946+tyrionzt@users.noreply.github.com>
2019-06-06 23:38:30 +0800

    注册留言板，书架到后台管理

--- resume/admin.py
+++ resume/admin.py
@@ -2,5 +2,14 @@
 from __future__ import unicode_literals
 
 from django.contrib import admin
+from .models import Message, Contact
 
 # Register your models here.
+class ContactInline(admin.TabularInline):
+    model = Contact
+
+class MessageAdmin(admin.ModelAdmin):
+    inlines = [ContactInline]
+
+admin.site.register(Message, MessageAdmin)
+admin.site.register(Contact)
